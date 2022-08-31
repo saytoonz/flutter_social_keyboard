bec932fd61fca194c895e881524f8ea3ec2a135b
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-08-31 18:35:16 +0000

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_social_keyboard/flutter_social_keyboard.dart';
+import 'package:flutter_social_keyboard/models/keyboard_config.dart';
+import 'package:flutter_social_keyboard/widgets/emoji_picker_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,10 +21,47 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: const FlutterSocialKeyboard(),
+        body: SafeArea(
+          child: FlutterSocialKeyboard(
+            keyboardConfig: KeyboardConfig(
+              columns: 9,
+              emojiSizeMax: 32 *
+                  (Platform.isIOS
+                      ? 1.30
+                      : 1.0), // Issue: https://github.com/flutter/flutter/issues/28894
+              verticalSpacing: 0,
+              horizontalSpacing: 0,
+              gridPadding: EdgeInsets.zero,
+              initCategory: Category.RECENT,
+              bgColor: const Color(0xFFF2F2F2),
+              indicatorColor: Colors.blue,
+              iconColor: Colors.grey,
+              iconColorSelected: Colors.blue,
+              progressIndicatorColor: Colors.blue,
+              backspaceColor: Colors.blue,
+              skinToneDialogBgColor: Colors.white,
+              skinToneIndicatorColor: Colors.grey,
+              enableSkinTones: true,
+              showRecentsTab: true,
+              recentsLimit: 28,
+              noRecents: const Text(
+                'No Recents',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black26,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              // tabIndicatorAnimDuration: kTabScrollDuration,
+              categoryIcons: const CategoryIcons(),
+              buttonMode: ButtonMode.MATERIAL,
+            ),
+          ),
+        ),
       ),
     );
   }
