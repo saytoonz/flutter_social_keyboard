9b89359af47a535d85609e441c5b5248471f02ed
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-08-31 09:40:09 +0000

    Adding EmojiPicker

--- lib/flutter_social_keyboard.dart
+++ lib/flutter_social_keyboard.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/material.dart';
 
 class FlutterSocialKeyboard extends StatefulWidget {
   const FlutterSocialKeyboard({Key? key}) : super(key: key);
@@ -10,6 +13,45 @@ class FlutterSocialKeyboard extends StatefulWidget {
 class _FlutterSocialKeyboardState extends State<FlutterSocialKeyboard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return EmojiPicker(
+      onEmojiSelected: (category, emoji) {
+        // Do something when emoji is tapped (optional)
+      },
+      onBackspacePressed: () {
+        // Do something when the user taps the backspace button (optional)
+      },
+      // textEditingController:
+      //     textEditionController, // pass here the same [TextEditingController] that is connected to your input field, usually a [TextFormField]
+      config: Config(
+        columns: 7,
+        emojiSizeMax: 32 *
+            (Platform.isIOS
+                ? 1.30
+                : 1.0), // Issue: https://github.com/flutter/flutter/issues/28894
+        verticalSpacing: 0,
+        horizontalSpacing: 0,
+        gridPadding: EdgeInsets.zero,
+        initCategory: Category.RECENT,
+        bgColor: Color(0xFFF2F2F2),
+        indicatorColor: Colors.blue,
+        iconColor: Colors.grey,
+        iconColorSelected: Colors.blue,
+        progressIndicatorColor: Colors.blue,
+        backspaceColor: Colors.blue,
+        skinToneDialogBgColor: Colors.white,
+        skinToneIndicatorColor: Colors.grey,
+        enableSkinTones: true,
+        showRecentsTab: true,
+        recentsLimit: 28,
+        // noRecents: const Text(
+        //   'No Recents',
+        //   style: TextStyle(fontSize: 20, color: Colors.black26),
+        //   textAlign: TextAlign.center,
+        // ),
+        // tabIndicatorAnimDuration: kTabScrollDuration,
+        categoryIcons: const CategoryIcons(),
+        buttonMode: ButtonMode.MATERIAL,
+      ),
+    );
   }
 }
