a6051dec4dcc35ac1348ff36f6ff21c5ad2ecc58
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-08-31 09:37:34 +0000

    Clean ups

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_social_keyboard/flutter_social_keyboard.dart';
 
 void main() {
@@ -16,37 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _flutterSocialKeyboardPlugin = FlutterSocialKeyboard();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _flutterSocialKeyboardPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -54,9 +20,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: const FlutterSocialKeyboard(),
       ),
     );
   }
--- lib/flutter_social_keyboard.dart
+++ lib/flutter_social_keyboard.dart
@@ -1,14 +1,15 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+import 'package:flutter/widgets.dart';
 
-import 'flutter_social_keyboard_platform_interface.dart';
+class FlutterSocialKeyboard extends StatefulWidget {
+  const FlutterSocialKeyboard({Key? key}) : super(key: key);
 
-class FlutterSocialKeyboard {
-  Future<String?> getPlatformVersion() {
-    return FlutterSocialKeyboardPlatform.instance.getPlatformVersion();
+  @override
+  State<FlutterSocialKeyboard> createState() => _FlutterSocialKeyboardState();
+}
+
+class _FlutterSocialKeyboardState extends State<FlutterSocialKeyboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
