c54e427cb619bc7825f60e13c9e2338906a9975d
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-01 16:50:39 +0000

    Create gif_picker_widget.dart

---
+++ lib/widgets/gif_picker_widget.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_keyboard/models/gif.dart';
+import 'package:flutter_social_keyboard/models/keyboard_config.dart';
+import 'package:flutter_social_keyboard/widgets/giphy_display.dart';
+
+class GifPickerWidget extends StatefulWidget {
+  final KeyboardConfig keyboardConfig;
+  final Function(GiphyGif)? onGifSelected;
+
+  const GifPickerWidget({
+    Key? key,
+    required this.keyboardConfig,
+    this.onGifSelected,
+  }) : super(key: key);
+
+  @override
+  State<GifPickerWidget> createState() => _GifPickerWidgetState();
+}
+
+class _GifPickerWidgetState extends State<GifPickerWidget>
+    with SingleTickerProviderStateMixin {
+  PageController? _pageController;
+  TabController? _tabController;
+  final double tabBarHeight = 46;
+  final int initCategory = 0;
+
+  late final List<String> _tabs = [
+    '',
+    'Trending',
+    ...widget.keyboardConfig.gifTabs
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(
+        initialIndex: initCategory, length: _tabs.length, vsync: this);
+
+    _pageController = PageController(initialPage: initCategory);
+    //   ..addListener(_closeSkinToneDialog);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      color: widget.keyboardConfig.bgColor,
+      child: Column(
+        children: [
+          SizedBox(
+            height: tabBarHeight,
+            child: TabBar(
+              isScrollable: _tabs.length > 4,
+              labelColor: widget.keyboardConfig.iconColorSelected,
+              indicatorColor: widget.keyboardConfig.indicatorColor,
+              unselectedLabelColor: widget.keyboardConfig.iconColor,
+              controller: _tabController,
+              labelPadding: _tabs.length > 4
+                  ? const EdgeInsets.symmetric(horizontal: 10)
+                  : EdgeInsets.zero,
+              onTap: (index) {
+                _pageController!.jumpToPage(index);
+              },
+              tabs: _tabs
+                  .asMap()
+                  .entries
+                  .map<Widget>((item) => _buildCategory(item.key, item.value))
+                  .toList(),
+            ),
+          ),
+          (widget.keyboardConfig.giphyAPIKey ?? "").isEmpty
+              ? const Text("No GIPHY API Key Found")
+              : Flexible(
+                  child: PageView.builder(
+                    itemCount: _tabs.length,
+                    controller: _pageController,
+                    onPageChanged: (index) {
+                      _tabController!.animateTo(
+                        index,
+                        duration:
+                            widget.keyboardConfig.tabIndicatorAnimDuration,
+                      );
+                    },
+                    itemBuilder: (context, index) => GiphyDisplay(
+                      searchKeyword: _tabs[index],
+                      keyboardConfig: widget.keyboardConfig,
+                      onGifSelected: widget.onGifSelected,
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategory(int index, String title) {
+    return Tab(
+      child: index == 0
+          ? const Icon(Icons.access_time)
+          : Text(
+              title,
+            ),
+    );
+  }
+}
