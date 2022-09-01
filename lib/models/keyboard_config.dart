a1a25b54885b08676d5fcb69c6056f4d4dcf4708
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-01 16:51:11 +0000

    Update keyboard_config.dart

--- lib/models/keyboard_config.dart
+++ lib/models/keyboard_config.dart
@@ -1,49 +1,53 @@
-import 'dart:math';
-
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
-
-const DefaultNoRecentsWidget = Text(
-  'No Recents',
-  style: TextStyle(fontSize: 20, color: Colors.black26),
-  textAlign: TextAlign.center,
-);
 
 /// KeyboardConfig for customizations
 class KeyboardConfig {
   /// Constructor
-  const KeyboardConfig(
-      {this.columns = 7,
-      this.emojiSizeMax = 32.0,
-      this.verticalSpacing = 0,
-      this.horizontalSpacing = 0,
-      this.gridPadding = EdgeInsets.zero,
-      this.initCategory = Category.RECENT,
-      this.bgColor = const Color(0xFFEBEFF2),
-      this.indicatorColor = Colors.blue,
-      this.iconColor = Colors.grey,
-      this.iconColorSelected = Colors.blue,
-      this.progressIndicatorColor = Colors.blue,
-      this.backspaceColor = Colors.blue,
-      this.skinToneDialogBgColor = Colors.white,
-      this.skinToneIndicatorColor = Colors.grey,
-      this.enableSkinTones = true,
-      this.showRecentsTab = true,
-      this.recentsLimit = 28,
-      this.replaceEmojiOnLimitExceed = false,
-      this.noRecents = DefaultNoRecentsWidget,
-      this.tabIndicatorAnimDuration = kTabScrollDuration,
-      this.categoryIcons = const CategoryIcons(),
-      this.buttonMode = ButtonMode.MATERIAL});
+  const KeyboardConfig({
+    this.giphyAPIKey,
+    this.gifTabs = const ['Haha', 'Sad', 'Love', 'Reaction'],
+    this.gifColumns = 3,
+    this.emojiColumns = 7,
+    this.emojiSizeMax = 32.0,
+    this.verticalSpacing = 0,
+    this.horizontalSpacing = 0,
+    this.gridPadding = EdgeInsets.zero,
+    this.initCategory = Category.RECENT,
+    this.bgColor = const Color(0xFFEBEFF2),
+    this.indicatorColor = Colors.blue,
+    this.iconColor = Colors.grey,
+    this.iconColorSelected = Colors.blue,
+    this.progressIndicatorColor = Colors.blue,
+    this.backspaceColor = Colors.blue,
+    this.skinToneDialogBgColor = Colors.white,
+    this.skinToneIndicatorColor = Colors.grey,
+    this.enableSkinTones = true,
+    this.showRecentsTab = true,
+    this.recentsLimit = 28,
+    this.replaceEmojiOnLimitExceed = false,
+    this.noRecents = const Text(
+      'No Recents',
+      style: TextStyle(fontSize: 20, color: Colors.black26),
+      textAlign: TextAlign.center,
+    ),
+    this.tabIndicatorAnimDuration = kTabScrollDuration,
+    this.categoryIcons = const CategoryIcons(),
+    this.buttonMode = ButtonMode.MATERIAL,
+  });
+
+  final String? giphyAPIKey;
+  final List<String> gifTabs;
+  final int gifColumns;
 
   /// Number of emojis per row
-  final int columns;
+  final int emojiColumns;
 
   /// Width and height the emoji will be maximal displayed
   /// Can be smaller due to screen size and amount of columns
   final double emojiSizeMax;
 
-  /// Verical spacing between emojis
+  /// Vertical spacing between emojis
   final double verticalSpacing;
 
   /// Horizontal spacing between emojis
@@ -103,88 +107,4 @@ class KeyboardConfig {
 
   /// Replace latest emoji on recents list on limit exceed
   final bool replaceEmojiOnLimitExceed;
-
-  /// Get Emoji size based on properties and screen width
-  double getEmojiSize(double width) {
-    final maxSize = width / columns;
-    return min(maxSize, emojiSizeMax);
-  }
-
-  /// Returns the icon for the category
-  IconData getIconForCategory(Category category) {
-    switch (category) {
-      case Category.RECENT:
-        return categoryIcons.recentIcon;
-      case Category.SMILEYS:
-        return categoryIcons.smileyIcon;
-      case Category.ANIMALS:
-        return categoryIcons.animalIcon;
-      case Category.FOODS:
-        return categoryIcons.foodIcon;
-      case Category.TRAVEL:
-        return categoryIcons.travelIcon;
-      case Category.ACTIVITIES:
-        return categoryIcons.activityIcon;
-      case Category.OBJECTS:
-        return categoryIcons.objectIcon;
-      case Category.SYMBOLS:
-        return categoryIcons.symbolIcon;
-      case Category.FLAGS:
-        return categoryIcons.flagIcon;
-      default:
-        throw Exception('Unsupported Category');
-    }
-  }
-
-  @override
-  bool operator ==(other) {
-    return (other is KeyboardConfig) &&
-        other.columns == columns &&
-        other.emojiSizeMax == emojiSizeMax &&
-        other.verticalSpacing == verticalSpacing &&
-        other.horizontalSpacing == horizontalSpacing &&
-        other.initCategory == initCategory &&
-        other.bgColor == bgColor &&
-        other.indicatorColor == indicatorColor &&
-        other.iconColor == iconColor &&
-        other.iconColorSelected == iconColorSelected &&
-        other.progressIndicatorColor == progressIndicatorColor &&
-        other.backspaceColor == backspaceColor &&
-        other.skinToneDialogBgColor == skinToneDialogBgColor &&
-        other.skinToneIndicatorColor == skinToneIndicatorColor &&
-        other.enableSkinTones == enableSkinTones &&
-        other.showRecentsTab == showRecentsTab &&
-        other.recentsLimit == recentsLimit &&
-        other.noRecents == noRecents &&
-        other.tabIndicatorAnimDuration == tabIndicatorAnimDuration &&
-        other.categoryIcons == categoryIcons &&
-        other.buttonMode == buttonMode &&
-        other.gridPadding == gridPadding &&
-        other.replaceEmojiOnLimitExceed == replaceEmojiOnLimitExceed;
-  }
-
-  @override
-  int get hashCode =>
-      columns.hashCode ^
-      emojiSizeMax.hashCode ^
-      verticalSpacing.hashCode ^
-      horizontalSpacing.hashCode ^
-      initCategory.hashCode ^
-      bgColor.hashCode ^
-      indicatorColor.hashCode ^
-      iconColor.hashCode ^
-      iconColorSelected.hashCode ^
-      progressIndicatorColor.hashCode ^
-      backspaceColor.hashCode ^
-      skinToneDialogBgColor.hashCode ^
-      skinToneIndicatorColor.hashCode ^
-      enableSkinTones.hashCode ^
-      showRecentsTab.hashCode ^
-      recentsLimit.hashCode ^
-      noRecents.hashCode ^
-      tabIndicatorAnimDuration.hashCode ^
-      categoryIcons.hashCode ^
-      buttonMode.hashCode ^
-      gridPadding.hashCode ^
-      replaceEmojiOnLimitExceed.hashCode;
 }
