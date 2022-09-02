fc5c3211b38e587382794e8c4ef89338cd19a074
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-02 18:56:32 +0000

    Update social_keyboard.dart

--- lib/src/social_keyboard.dart
+++ lib/src/social_keyboard.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_social_keyboard/models/gif.dart';
 import 'package:flutter_social_keyboard/models/keyboard_config.dart';
 import 'package:flutter_social_keyboard/widgets/emoji_picker_widget.dart';
@@ -25,88 +28,125 @@ class FlutterSocialKeyboard extends StatefulWidget {
 }
 
 class _FlutterSocialKeyboardState extends State<FlutterSocialKeyboard> {
+  final StreamController<String> scrollStream =
+      StreamController<String>.broadcast();
   int _currentIndex = 0;
+  bool _showBottomNav = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      scrollStream.stream.listen((event) {
+        if (event == "hideNav") {
+          if (_showBottomNav) {
+            setState(() {
+              _showBottomNav = false;
+            });
+          }
+        } else {
+          if (!_showBottomNav) {
+            setState(() {
+              _showBottomNav = true;
+            });
+          }
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: IndexedStack(
-            index: _currentIndex,
-            children: [
-              //Emoji
-              EmojiPickerWidget(
-                keyboardConfig: widget.keyboardConfig,
-                onBackspacePressed: widget.onBackspacePressed,
-                onEmojiSelected: widget.onEmojiSelected,
-              ),
-              //Gif
-              GifPickerWidget(
-                keyboardConfig: widget.keyboardConfig,
-                onGifSelected: widget.onGifSelected,
-              ),
-              Container(),
-            ],
-          ),
-        ),
-        //Bottom navigation
-        Container(
-          height: 50,
-          padding: EdgeInsets.zero,
-          decoration: BoxDecoration(
-            color: widget.keyboardConfig.bgColor,
-            boxShadow: const [
-              BoxShadow(
-                color: Color.fromRGBO(43, 52, 69, .1),
-                offset: Offset(0, -5),
-                spreadRadius: 10,
-                blurRadius: 200,
-              ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return SafeArea(
+      bottom: widget.keyboardConfig.withSafeArea && _showBottomNav,
+      top: widget.keyboardConfig.withSafeArea,
+      left: widget.keyboardConfig.withSafeArea,
+      right: widget.keyboardConfig.withSafeArea,
+      child: Column(
+        children: [
+          Expanded(
+            child: IndexedStack(
+              index: _currentIndex,
               children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                  ),
+                //Emoji
+                EmojiPickerWidget(
+                  keyboardConfig: widget.keyboardConfig,
+                  onBackspacePressed: widget.onBackspacePressed,
+                  onEmojiSelected: widget.onEmojiSelected,
                 ),
-                const Spacer(),
-                _getImgIcon(
-                  image: "icons8-emoji-96.png",
-                  index: 0,
+                //Gif
+                GifPickerWidget(
+                  keyboardConfig: widget.keyboardConfig,
+                  onGifSelected: widget.onGifSelected,
+                  scrollStream: scrollStream,
                 ),
-                const SizedBox(
-                  width: 15,
-                ),
-                _getImgIcon(
-                  image: "icons8-gif-96.png",
-                  size: 26,
-                  index: 1,
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                _getImgIcon(image: "icons8-sticker-100.png", index: 2),
-                const Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.backspace_outlined,
-                  ),
-                ),
+                Container(),
               ],
             ),
           ),
-        ),
-      ],
+          //Bottom navigation
+          Visibility(
+            visible: _showBottomNav,
+            child: Container(
+              height: 50,
+              padding: EdgeInsets.zero,
+              decoration: BoxDecoration(
+                color: widget.keyboardConfig.bgColor,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(43, 52, 69, .1),
+                    offset: Offset(0, -5),
+                    spreadRadius: 10,
+                    blurRadius: 200,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                      ),
+                    ),
+                    const Spacer(),
+                    _getImgIcon(
+                      image: "icons8-emoji-96.png",
+                      index: 0,
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    _getImgIcon(
+                      image: "icons8-gif-96.png",
+                      size: 26,
+                      index: 1,
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    _getImgIcon(image: "icons8-sticker-100.png", index: 2),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.backspace_outlined,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
