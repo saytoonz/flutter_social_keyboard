e31d2e6a9cf3edc6ec428ce5b5ec573f117cc04f
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-04 07:36:16 +0000

    Update giphy_display.dart

--- lib/widgets/giphy_display.dart
+++ lib/widgets/giphy_display.dart
@@ -4,8 +4,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter_social_keyboard/flutter_social_keyboard.dart';
+
 import 'package:flutter_social_keyboard/models/collection.dart';
+import 'package:flutter_social_keyboard/models/gif.dart';
+import 'package:flutter_social_keyboard/models/keyboard_config.dart';
 import 'package:flutter_social_keyboard/resources/client.dart';
 
 class GiphyDisplay extends StatefulWidget {
