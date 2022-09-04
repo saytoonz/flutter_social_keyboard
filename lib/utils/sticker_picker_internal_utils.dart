70c3e53aa8bfac71c6c94254e761d7b498d754da
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-04 11:32:19 +0000

    Update sticker_picker_internal_utils.dart

--- lib/utils/sticker_picker_internal_utils.dart
+++ lib/utils/sticker_picker_internal_utils.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+
+import 'package:flutter_social_keyboard/models/sticker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class StickerPickerInternalUtils {
   /// Returns list of recently used emoji from cache
-  Future<List<RecentEmoji>> getRecentEmojis() async {
+  Future<List<Sticker>> getRecentStickers() async {
     final prefs = await SharedPreferences.getInstance();
-    var emojiJson = prefs.getString('recent');
-    if (emojiJson == null) {
+    var sticker = prefs.getString('recent_stickers');
+    if (sticker == null) {
       return [];
     }
-    var json = jsonDecode(emojiJson) as List<dynamic>;
-    return json.map<RecentEmoji>(RecentEmoji.fromJson).toList();
+    var json = jsonDecode(sticker) as List<dynamic>;
+    return json.map<Sticker>(Sticker.fromJson).toList();
   }
 }
