1ac41d07151ef7bd2f4b54345d7423bf8a994d5c
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-04 11:32:07 +0000

    Update flutter_social_keyboard.dart

--- lib/flutter_social_keyboard.dart
+++ lib/flutter_social_keyboard.dart
@@ -4,3 +4,4 @@ export 'package:flutter_social_keyboard/src/social_keyboard.dart';
 export 'package:flutter_social_keyboard/models/keyboard_config.dart';
 export 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 export 'package:flutter_social_keyboard/models/gif.dart';
+export 'package:flutter_social_keyboard/models/sticker.dart';
