b160ce24dfc649316b7a9c030e537b5ade458ee2
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-04 12:09:13 +0000

    Update sticker_display.dart

--- lib/widgets/sticker_display.dart
+++ lib/widgets/sticker_display.dart
@@ -5,10 +5,10 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_social_keyboard/models/keyboard_config.dart';
 import 'package:flutter_social_keyboard/models/sticker.dart';
-import 'package:flutter_social_keyboard/models/sticker_model.dart';
+import 'package:flutter_social_keyboard/models/stickers_model.dart';
 
 class StickerDisplay extends StatefulWidget {
-  final StickerModel stickerModel;
+  final StickersModel stickerModel;
   final KeyboardConfig keyboardConfig;
   final Function(Sticker)? onStickerSelected;
   final StreamController<String> scrollStream;
@@ -49,34 +49,32 @@ class _StickerDisplayState extends State<StickerDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return widget.stickerModel.assetUrls.isEmpty
+    return widget.stickerModel.stickers.isEmpty
         ? const Center(
             child: Text(
               "There was an error, try again!",
             ),
           )
         : GridView.builder(
-            itemCount: widget.stickerModel.assetUrls.length,
+            itemCount: widget.stickerModel.stickers.length,
             scrollDirection: Axis.vertical,
             controller: _scrollController,
             padding: widget.keyboardConfig.gridPadding,
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: widget.keyboardConfig.stickerColumns,
-                crossAxisSpacing:
-                    widget.keyboardConfig.stickerHorizontalSpacing,
-                mainAxisSpacing: widget.keyboardConfig.stickerVerticalSpacing),
+              crossAxisCount: widget.keyboardConfig.stickerColumns,
+              crossAxisSpacing: widget.keyboardConfig.stickerHorizontalSpacing,
+              mainAxisSpacing: widget.keyboardConfig.stickerVerticalSpacing,
+            ),
             itemBuilder: (context, index) {
               return InkWell(
                 onTap: () {
                   if (widget.onStickerSelected != null) {
-                    widget.onStickerSelected!(Sticker(
-                      assetUrl: widget.stickerModel.assetUrls[index],
-                      category: widget.stickerModel.title,
-                    ));
+                    widget.onStickerSelected!(
+                        widget.stickerModel.stickers[index]);
                   }
                 },
                 child: Image.asset(
-                  widget.stickerModel.assetUrls[index],
+                  widget.stickerModel.stickers[index].assetUrl,
                   errorBuilder: ((context, error, stackTrace) =>
                       const Icon(Icons.error)),
                   fit: BoxFit.cover,
