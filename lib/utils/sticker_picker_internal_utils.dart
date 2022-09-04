9ca7612c56066b4ebb5327d67de43901b621c2db
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-09-04 11:06:58 +0000

    Create sticker_picker_internal_utils.dart

---
+++ lib/utils/sticker_picker_internal_utils.dart
@@ -0,0 +1,12 @@
+class StickerPickerInternalUtils {
+  /// Returns list of recently used emoji from cache
+  Future<List<RecentEmoji>> getRecentEmojis() async {
+    final prefs = await SharedPreferences.getInstance();
+    var emojiJson = prefs.getString('recent');
+    if (emojiJson == null) {
+      return [];
+    }
+    var json = jsonDecode(emojiJson) as List<dynamic>;
+    return json.map<RecentEmoji>(RecentEmoji.fromJson).toList();
+  }
+}
